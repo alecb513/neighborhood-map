a80b2c866c0a8e816d6ef4af6770338287eeebc1
Alec Botian <abotian@gmail.com>
2018-11-20 20:36:24 -0800

    still working on map

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,16 +3,14 @@ import {GoogleApiWrapper} from 'google-maps-react';
 
 // ...
 
-export class Map extends React.Component {
+class Map extends Component {
 
-render(){
-    return(
-        <div id='map'></div>
+    render() {
+      return(
+        <div>
+          <Map initialCenter={lat: 12345, lng: 678910} zoom={13}/>
+        </div>
     )
-}
-
-}
-
-export default GoogleApiWrapper({
-  apiKey: ("AIzaSyBz491EHlYacMDqaP4b_nHSskvEfzLxj7c")
-})(Map)
+  }
+  
+  export default GoogleApiWrapper({ apiKey: "AIzaSyBz491EHlYacMDqaP4b_nHSskvEfzLxj7c" })(DisplayMap);
