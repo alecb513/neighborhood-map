2470aa58753b983565e3dce63ced38d3bd5d7459
Alec Botian <abotian@gmail.com>
2018-11-20 22:50:33 -0800

    Maps working

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 //import './components/Map'
-import Map from './components/Map'
+import DisplayMap from './components/Map'
 
 class App extends Component {
 
- 
+
 
 
   render() {
     return (
-      <div>Hello App
-          <Map />
+      <div>
+        <h1>Google Map should load here</h1>
+        <DisplayMap />
       </div>
     )
   }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
-import {GoogleApiWrapper} from 'google-maps-react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
 // ...
 
-class Map extends Component {
+class DisplayMap extends Component {
 
-    render() {
-      return(
-        <div>
-          <Map initialCenter={{lat: 40.7713024, lng: -73.9632393}} zoom={13}/>
-        </div>
+  render() {
+    const style = {
+      height: '100%',
+      width: '100%'
+    }
+
+    return(
+      <div>
+        <Map
+        google={this.props.google}
+        initialCenter={{lat: 40.7713024, lng: -73.9632393}}
+        zoom={13}
+        style={style}>
+        </Map>
+      </div>
     )
   }
 }
-  export default Map({ apiKey: "AIzaSyBz491EHlYacMDqaP4b_nHSskvEfzLxj7c" })(Map);
+  export default GoogleApiWrapper({ apiKey: "AIzaSyBz491EHlYacMDqaP4b_nHSskvEfzLxj7c" })(DisplayMap);
