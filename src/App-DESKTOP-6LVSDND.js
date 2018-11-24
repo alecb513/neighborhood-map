7e78e67d51208f5bb7e5870a48250d4cc573ded8
Alec Botian <abotian@gmail.com>
2018-11-24 11:43:43 -0800

    evertything is cool this time

--- src/App-DESKTOP-6LVSDND.js
+++ src/App-DESKTOP-6LVSDND.js
@@ -2,22 +2,20 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 //import './components/Map'
-import Map from './components/Map'
+import DisplayMap from './components/Map'
 
 class App extends Component {
 
-  state = {};
+
 
 
   render() {
     return (
-      <div>Hello App
-          <Map />
+      <div>
+        <h1>Google Map should load here</h1>
+        <DisplayMap />
       </div>
     )
   }
 }
 export default App;
-
-
-
